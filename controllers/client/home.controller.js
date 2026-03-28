7a8d8f068abbf4cbe774dbca971e00edbd3dfff3
Anthai2 <vo.anthai142@gmail.com>
2026-03-28 17:12:18 +0700

    feat(project): update UI & synchronize workflow

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,5 @@
 exports.index = (req, res) => {
   res.render('client/pages/home/index', {
-    title: 'ProjX | Architectural Workspace & Team Productivity',
+    title: 'Taskify | Team Workspace & Project Management',
   });
 };
